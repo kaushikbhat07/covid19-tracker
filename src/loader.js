434d1e6c3834c6001b8be0e393f75936882a7a65
Kaushik Bhat <kaushik.bantval98@gmail.com>
2020-05-05 11:50:34 +0530

    Goto top jquery

--- src/loader.js
+++ src/loader.js
@@ -3,4 +3,19 @@ import $ from 'jquery';
 $(window).on('load', function () {
 	$('#loader').fadeOut();
 	$('#loader-icon').fadeOut();
+
+	$(".goto-top-btn").fadeOut();
+	
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 300) {
+			// Show back to top button
+			$(".goto-top-btn").fadeIn();
+
+		} else {
+			// Hide back to top button
+			$(".goto-top-btn").fadeOut();
+		}
+	})
+
+
 });
